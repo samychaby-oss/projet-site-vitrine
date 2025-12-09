163921e43dc10a245da0920d5656c300618a3114
Wesley <wes@Air-Wes.local>
2025-12-09 12:59:26 +0100

    Ajout balise <a> dans la nav bar

--- discover/index.js
+++ discover/index.js
@@ -15,12 +15,12 @@ folders.forEach((folder) => {
     console.log(folders.click);
     let A = folders.click;
     console.log(A);
-    if (A === ".folder-2") {
-      folderGroup.classList.add("level-2-active");
-    }
+    // if (A === ".folder-2") {
+    //   folderGroup.classList.add("level-2-active");
+    // }
     // folderGroup.classList.add("level-1-active");
     // folderGroup.classList.add("level-2-active");
-    // folderGroup.classList.add("level-3-active");
+    folderGroup.classList.add("level-3-active");
     // folderGroup.classList.add("level-4-active");
   });
 });
